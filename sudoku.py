61de73d44774fe752d4fb1e1e19a32bc9eb2823d
nw <nico.walters@techtalents.es>
2020-06-05 19:14:19 +0200

    More

--- sudoku.py
+++ sudoku.py
@@ -26,3 +26,22 @@ def print_puzzle(puzzle):
         output += "|\n"
     output += "-" * 21
     print(output)
+
+def check_pos(puzzle, x, y, n):
+    row = puzzle[y]
+    column = [row[x] for row in puzzle]
+
+    i = (x // 3) * 3
+    j = (y // 3) * 3
+    section = [n for row in puzzle[j : j+3] for n in row[i : i+3]]
+    
+    return not n in row and not n in column and not n in section
+    
+# heuristics
+# backtracking
+
+
+puzzles = read_puzzle_file()
+puzzle = convert_puzzle(puzzles[18])
+
+print_puzzle(puzzle)
